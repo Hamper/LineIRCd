1000a9aa63ee6351493818d7a7c2914454455814
hamper <hamper.nhs@gmail.com>
2016-06-29 16:02:56 +0300

    fix ipv6 connection problems

--- src/ssl.c
+++ src/ssl.c
@@ -218,12 +218,13 @@ void init_ctx_server(void)
 
 void init_ctx_client(void)
 {
-	ctx_client = SSL_CTX_new(SSLv3_client_method());
+	ctx_client = SSL_CTX_new(SSLv23_client_method());
 	if (!ctx_client)
 	{
 		ircd_log(LOG_ERROR, "Failed to do SSL CTX new client");
 		exit(2);
 	}
+	SSL_CTX_set_options(ctx_client, SSL_OP_NO_SSLv2);
 	SSL_CTX_set_default_passwd_cb(ctx_client, ssl_pem_passwd_cb);
 	SSL_CTX_set_session_cache_mode(ctx_client, SSL_SESS_CACHE_OFF);
 	if (SSL_CTX_use_certificate_file(ctx_client, SSL_SERVER_CERT_PEM, SSL_FILETYPE_PEM) <= 0)
